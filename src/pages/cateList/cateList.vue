98575db73f6c06a111044f6217d27cbb51276ba4
howe <2323162636@qq.com>
2020-05-08 22:02:44 +0800

    分类完成

--- src/pages/cateList/cateList.vue
+++ src/pages/cateList/cateList.vue
@@ -11,16 +11,24 @@
     <!-- 内容去 -->
     <div class="wrapper" >
       <!-- 左侧导航 -->
-  		<div class="left" ref="left">
+  		<div class="left" >
         <ul class="content" >
-    		<li class="item" v-for="(item,index) in cateNavData.categoryL1List " :key="index">
-          <span>{{item.name}}</span>
-        </li>
-  		</ul>
+          <li class="item" v-for="(item,index) in cateNavData.categoryL1List " :key="index" @click='changeCurrent(index)' :class='currentIndex===index?"active":""'>
+            <span>{{item.name}}</span>
+          </li>
+  		  </ul>
       </div>
       <!-- 右侧图片 -->
-      <div class="right">
-
+      <div class="right" ref="right">
+        <div class="subCateContainer">
+          <div class="banner"><img src="https://yanxuan.nosdn.127.net/a41ddf5b8d7921d5d09987022dd71cac.jpg?quality=75&type=webp&imageView&thumbnail=0x196" alt=""></div>
+          <div class="catelist" v-if='cateList.length>0'>
+              <div class="cateItem" v-for="(cateItem,index) in cateList[currentIndex].categoryList?cateList[currentIndex].categoryList:cateList[currentIndex].subCateList" :key="index">
+                <img :src="cateItem.bannerUrl?cateItem.bannerUrl:cateItem.wapBannerUrl" alt="">
+                <span class="name">{{cateItem.name}}</span>
+              </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -33,14 +41,22 @@ import http from '../../http/index'
 export default {
   data(){
     return{
-      cateNavData:{}
+      cateNavData:{},
+      cateList:[],
+      currentIndex:0
     }
   },
   async mounted(){
     this.$nextTick(()=>{
-      new BScroll(this.$refs.left,{scrollY:true,click:true})
+      new BScroll(this.$refs.right,{scrollY:true,click:true})
     })
     this.cateNavData = await http.index.getcateNavData()
+    this.cateList = await http.index.getCateListData()
+  },
+  methods:{
+    changeCurrent(index){
+      this.currentIndex=index;
+    }
   }
 }
 </script>
@@ -85,21 +101,40 @@ export default {
           boder none 
           font-size 28px
           margin 40px 0
-          &.avtive:after
-            content ''
-            position absolute
-            top 0
-            left 0
-            bottom 0
-            width 6px
-            height 50px
-            background-color #ab2b2b
+          &.active
+            &::after
+              content ''
+              position absolute
+              top 0
+              left 0
+              width 6px
+              height 50px
+              background-color #ab2b2b
           span  
-            color #ab2b2b
             line-height 50px
     .right
       padding 30px 30px 21px
       height 100%
       width 588px
       overflow hidden
+      .subCateContainer
+        .banner
+          img 
+            width 528px
+            height 192px
+        .catelist
+          display flex
+          flex-wrap wrap
+          width 528px
+          .cateItem
+            width 144px
+            height 216px
+            margin-right 20px
+            display flex
+            flex-direction column
+            align-items center
+            &>img
+              width 144px
+              height 144px
+
 </style>
